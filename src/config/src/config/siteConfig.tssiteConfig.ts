6d08b58453c4dacdee9e8079906a5a0a0da94735
garimpofc30-blip <garimpofc30@gmail.com>
2026-07-29 16:18:35 -0300

    Create siteConfig.tssiteConfig.ts

---
+++ src/config/src/config/siteConfig.tssiteConfig.ts
@@ -0,0 +1,22 @@
+export const siteConfig = {
+  name: "Garimpo Futebol Clube",
+  shortName: "Garimpo F.C.",
+  description: "Site oficial do Garimpo Futebol Clube. A raça e o orgulho da nossa comunidade.",
+  url: "https://garimpofc.com.br",
+  ogImage: "/images/escudo.png",
+  links: {
+    instagram: "https://instagram.com/garimpofc",
+    facebook: "https://facebook.com/garimpofc",
+  },
+  navigation: [
+    { name: "Início", href: "/" },
+    { name: "O Clube", href: "/clube" },
+    { name: "Notícias", href: "/noticias" },
+    { name: "Jogos", href: "/jogos" },
+    { name: "Elenco", href: "/elenco" },
+    { name: "Loja", href: "/loja" },
+    { name: "Rifas", href: "/rifas" },
+    { name: "Patrocinadores", href: "/patrocinadores" },
+    { name: "Contato", href: "/contato" },
+  ],
+};
